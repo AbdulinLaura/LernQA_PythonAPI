c8d075a04d93573a560f1e8bf745c1c56123becd
AbdulinLaura <79696059+AbdulinLaura@users.noreply.github.com>
2023-01-13 17:54:50 +0200

    Ex10: Тест на короткую фразу

--- Homework7.py
+++ Homework7.py
@@ -1,5 +1,5 @@
 class Test_example:
     def test_check_math(self):
         a = str(input())
-        expected_result = len(a) < 15
+        expected_result = len(a)
         assert expected_result < 15, f"The entered value {expected_result} is greater than 15"
